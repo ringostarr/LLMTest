84c65fc9d14fd15f9d911058a35925c11c29f796
Ringostarr <akshay@fspl.in>
2025-07-08 10:31:52 -0400

    Update ft.py

--- LLMTest/ft.py
+++ LLMTest/ft.py
@@ -1,5 +1,7 @@
 #Idea is to fine-tune and change the "thou" to "you" and maybe "arth" to are
-
+#classic transformer , big thanks to andrej karthay
+# - Akshay
+# Jan 2024
 import torch
 print(torch.cuda.is_available())  # Should return True
 
